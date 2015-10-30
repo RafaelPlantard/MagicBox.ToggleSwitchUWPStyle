d7d42a86479d93f5365bc35c45a69a69f6efd71e
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2015-10-30 16:58:48 -0200

    First version to commit.

---
+++ MagicBox.ToggleSwitchUWPStyle/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace MagicBox.ToggleSwitchUWPStyle
+{
+    /// <summary>
+    /// The code-behind for <see cref="MainPage"/>.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        /// <summary>
+        /// Initializes the page components.
+        /// </summary>
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
